b13a5a2eb443e0da418dee3d3c79eff7cc3deeac
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-02-24 17:44:43 -0300

    finalizando dia 3 do nlw4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,17 @@
-import styles from '../styles/components/Profile.module.css';
+import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
-    return (
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/igor-lisboa.png" alt="Igor Lisbôa" />
-            <div>
-                <strong>Igor Lisbôa</strong>
-                <p>
-                    <img src="icons/level.svg" alt="Level" />
-                    Level 1
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/igor-lisboa.png" alt="igor-lisboa"/>
+      <div>
+        <strong>igor-lisboa</strong>
+        
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1
+        </p>
+      </div>
+    </div>
+  )
 }
